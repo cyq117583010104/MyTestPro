6c452cf5ca656b483e71bc90d30b73e3c22b0ffc
cyq117583010104 <496296303@qq.com>
2020-06-08 23:03:40 +0800

    github第一次测试！

---
+++ src/HelloYww.java
@@ -0,0 +1,13 @@
+/**
+ * author  cyq
+ * date  2020/5/31 - 15:26
+ */
+public class HelloYww {
+    public static void main(String[] args) {
+        System.out.println("殷伟伟吃屎啊！");
+        System.out.println("殷伟伟吃屎啊！");
+        System.out.println("殷伟伟吃屎啊！");
+        System.out.println("殷伟伟吃屎啊！");
+
+    }
+}
